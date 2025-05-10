0248f5f5e0e8e4b3fdb1e647a5528bf9b63fd07e
t-houssian <tylerhoussian@gmail.com>
2025-05-10 15:32:20 -0500

    push favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
+// pages/_app.js
+import Head from 'next/head'
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        {/* Points at /public/favicon.ico */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
